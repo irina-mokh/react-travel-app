50bb6ca554fe4ff5b97de700c996f9fc5b152f88
irina-mokh <irina.mokh.spb@gmail.com>
2022-03-24 14:21:30 +0300

    feat: add searchbar component

---
+++ components/src/components/SearchBar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+interface SearchProps {
+  value: string;
+}
+
+interface SearchState {
+  value: string;
+}
+
+class SearchBar extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: e.target?.value });
+    localStorage.setItem('search', e.target.value);
+  }
+
+  render() {
+    return (
+      <input
+        type="search"
+        className="search"
+        onChange={this.handleChange}
+        placeholder="Enter a country"
+        value={this.state.value}
+      />
+    );
+  }
+}
+
+export default SearchBar;
--- components/src/pages/Homepage.tsx
+++ components/src/pages/Homepage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        <main>
+        <main className="main">
           <h2>Welcome to the homepage!</h2>
           <p>You can do this, I believe in you.</p>
+          <SearchBar value={localStorage.search || null} />
         </main>
       </>
     );
