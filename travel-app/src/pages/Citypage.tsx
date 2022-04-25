b573ffc170512c69f8908978b337ba5a3b21c7af
irina-mokh <irina.mokh.spb@gmail.com>
2022-04-25 13:51:44 +0300

    feat: add a map to city page

--- travel-app/src/pages/Citypage.tsx
+++ travel-app/src/pages/Citypage.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import GoogleMapReact from 'google-map-react';
+import ReactMapboxGl from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { Navigate } from 'react-router-dom';
 import { CitiesStore } from '../store/cities';
+import { Link } from 'react-router-dom';
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1Ijoic3VwZXJtb2giLCJhIjoiY2wyZWhxN3o5MDB1MzNibWsza2pma2kwdyJ9.zhR6EvEjA9dn7RINZU2Jww',
+});
 
 export const City = () => {
   const {
@@ -14,7 +21,7 @@ export const City = () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setTime(data.data.slice(0, 8));
+        setTime(data.data.slice(0, 5));
       });
   };
 
@@ -27,57 +34,56 @@ export const City = () => {
 
     content = (
       <div className="city-page">
-        <h4 className="city-page__title">{name}</h4>
-        <p className="city-page__country">{country}</p>
-        <div className="city-page__content">
-          <table className="city-page__table">
-            <tbody>
-              <tr>
-                <td>Region: </td>
-                <td>{region}</td>
-              </tr>
-              <tr>
-                <td>Region code: </td>
-                <td>{regionCode}</td>
-              </tr>
-              <tr>
-                <td>Population: </td>
-                <td>{population.toLocaleString()}</td>
-              </tr>
-              <tr>
-                <td>Time: </td>
-                <td>{time}</td>
-              </tr>
-              <tr></tr>
-              <tr>
-                <td>WikiData: </td>
-                <td>
-                  <a href={`http://www.wikidata.org/entity/${wikiDataId}`}>{name}</a>
-                </td>
-              </tr>
-              <tr>
-                <td>Latitude: </td>
-                <td>{latitude}</td>
-              </tr>
-              <tr>
-                <td>Longitude: </td>
-                <td>{longitude}</td>
-              </tr>
-            </tbody>
-          </table>
-          <div
-            style={{
-              width: '50%',
-              height: '350px',
-            }}
-          >
-            <GoogleMapReact
-              defaultZoom={8}
-              defaultCenter={{
-                lat: latitude,
-                lng: longitude,
-              }}
-            />
+        <nav className="city-page__nav">
+          <Link className="city-page__link" to="/cities">
+            &#10094; Back to results
+          </Link>
+        </nav>
+        <div className="city-page__main">
+          <h4 className="city-page__title">{name}</h4>
+          <p className="city-page__country">{country}</p>
+          <div className="city-page__content">
+            <table className="city-page__table">
+              <tbody>
+                <tr>
+                  <td>Region: </td>
+                  <td>{region}</td>
+                </tr>
+                <tr>
+                  <td>Region code: </td>
+                  <td>{regionCode}</td>
+                </tr>
+                <tr>
+                  <td>Population: </td>
+                  <td>{population.toLocaleString()}</td>
+                </tr>
+                <tr>
+                  <td>Time: </td>
+                  <td>{time}</td>
+                </tr>
+                <tr></tr>
+                <tr>
+                  <td>WikiData: </td>
+                  <td>
+                    <a href={`http://www.wikidata.org/entity/${wikiDataId}`}>{name}</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Latitude: </td>
+                  <td>{latitude}</td>
+                </tr>
+                <tr>
+                  <td>Longitude: </td>
+                  <td>{longitude}</td>
+                </tr>
+              </tbody>
+            </table>
+            <Map
+              className="city-page__map"
+              style="mapbox://styles/mapbox/streets-v9"
+              center={[longitude, latitude]}
+              zoom={[10]}
+            ></Map>
           </div>
         </div>
       </div>
